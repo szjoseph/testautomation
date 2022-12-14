e2eb190e99aeac7ce64a773db58cb763540f071c
szjoseph <szakal.j.david@gmail.com>
2022-12-14 01:54:21 +0100

    registration feature, first scenario done

--- src/test/java/hu/masterfield/steps/TescoSteps.java
+++ src/test/java/hu/masterfield/steps/TescoSteps.java
@@ -1,16 +1,26 @@
 package hu.masterfield.steps;
 
+import com.codeborne.selenide.Configuration;
 import hu.masterfield.pages.HomePage;
+import hu.masterfield.pages.RegistrationPage;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import static com.codeborne.selenide.Selenide.open;
+import static org.junit.Assert.assertEquals;
+
 
 public class TescoSteps {
+
     @Given("open main page")
     public void openMainPage() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-blink-features=AutomationControlled");
+        Configuration.browserCapabilities = options;
+
         open("https://bevasarlas.tesco.hu/groceries/hu-HU");
     }
 
@@ -31,5 +41,58 @@ public class TescoSteps {
     public void theCookiePanelCloses() {
         HomePage homePage = new HomePage();
         homePage.cookiePanelNotVisible();
+    }
+
+    @Then("the user is on the sign up page")
+    public void theUserIsOnTheSignUpPage() {
+        HomePage homePage = new HomePage();
+        homePage.clickRegistrationBtn();
+
+        RegistrationPage registrationPage = new RegistrationPage();
+        assertEquals("1", registrationPage.stageNumber.getText());
+    }
+
+
+    @And("click on submit button")
+    public void clickOnSubmitButton() throws InterruptedException {
+        RegistrationPage registrationPage = new RegistrationPage();
+        registrationPage.clickSubmit();
+    }
+
+    @When("the user fills out the <email>, <password>, <password confirmation>")
+    public void theUserFillsOutTheEmailPasswordPasswordConfirmation() {
+        RegistrationPage registrationPage = new RegistrationPage();
+        registrationPage.setEmailPass();
+        assertEquals("1", registrationPage.stageNumber.getText());
+    }
+
+    @Then("the user should be able to access their new account")
+    public void theUserShouldBeAbleToAccessTheirNewAccount() {
+        RegistrationPage registrationPage = new RegistrationPage();
+        registrationPage.validateRegSuccess();
+    }
+
+    @And("selects <county>, <municipality>, <postal code>, <street>, <house number>, <address name>")
+    public void selectsCountyMunicipalityPostalCodeStreetHouseNumberAddressName() throws InterruptedException {
+        RegistrationPage registrationPage = new RegistrationPage();
+        registrationPage.setRegStage2();
+    }
+
+    @And("enter <first name>, <last name>, <phone number>")
+    public void enterFirstNameLastNamePhoneNumber() {
+        RegistrationPage registrationPage = new RegistrationPage();
+        registrationPage.setRegStage3();
+    }
+
+    @And("accept registration consent")
+    public void acceptRegistrationConsent() {
+        RegistrationPage registrationPage = new RegistrationPage();
+        registrationPage.regConstCheckBox();
+    }
+
+    @And("click on <registration submit> button")
+    public void clickOnRegistrationSubmitButton() {
+        RegistrationPage registrationPage = new RegistrationPage();
+        registrationPage.finalizeReg();
     }
 }
