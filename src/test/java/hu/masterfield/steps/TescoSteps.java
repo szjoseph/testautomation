4c0f1600aefa951c66992cf3572bec2c7376b80b
szjoseph <szakal.j.david@gmail.com>
2022-12-15 16:14:10 +0100

    added browser size configuration

--- src/test/java/hu/masterfield/steps/TescoSteps.java
+++ src/test/java/hu/masterfield/steps/TescoSteps.java
@@ -27,6 +27,8 @@ public class TescoSteps {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-blink-features=AutomationControlled");
         Configuration.browserCapabilities = options;
+        //   options.setHeadless(true);
+        Configuration.browserSize = "1920x1080";
     }
 
     @After
