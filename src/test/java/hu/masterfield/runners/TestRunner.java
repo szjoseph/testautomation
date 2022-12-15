b815423beeda7df970509fdfccd169e82d7b10d3
szjoseph <szakal.j.david@gmail.com>
2022-12-15 12:53:16 +0100

    fixed local html report

--- src/test/java/hu/masterfield/runners/TestRunner.java
+++ src/test/java/hu/masterfield/runners/TestRunner.java
@@ -10,8 +10,10 @@ import org.junit.runner.RunWith;
         //        features = "@target/cucumber-rerun.txt"
         publish = true,
         glue = {"hu.masterfield.steps"},
-        plugin = {"pretty", "json:target/cucumber-reports.json",
-                "junit:target/cucumber-reports.html",
+        plugin = {"pretty",
+                "html:target/cucumber-reports.html",
+                "json:target/cucumber-reports.json",
+            //    "junit:target/cucumber-reports.html",
                 "rerun:target/cucumber-rerun.txt",
                 "junit:target/cucumber-rep"})
 public class TestRunner {
