b65c4033a70f6ac017e8c101b23a88d17d032a1b
szjoseph <szakal.j.david@gmail.com>
2022-12-15 16:14:52 +0100

    changed locator, added waiting time

--- src/test/java/hu/masterfield/pages/ProductDetailsPage.java
+++ src/test/java/hu/masterfield/pages/ProductDetailsPage.java
@@ -1,14 +1,17 @@
 package hu.masterfield.pages;
 
+import java.time.Duration;
+
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
 public class ProductDetailsPage {
     public void clickProduct() {
-        $("#tile-2004120331802").shouldBe(visible).click();
+        //   $("#tile-2004120331802").shouldBe(visible, Duration.ofSeconds(10)).click();
+        $("#tile-2004120331802 > a > div > picture > img").shouldBe(visible, Duration.ofSeconds(10)).click();
     }
 
     public void validateDetailsPage() {
-        $(".product-details-tile").shouldBe(visible);
+        $(".product-details-tile").shouldBe(visible, Duration.ofSeconds(10));
     }
 }
