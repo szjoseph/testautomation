128beba50272497a10582b008898e39fb66e3657
szjoseph <szakal.j.david@gmail.com>
2022-12-16 10:50:27 +0100

    added wait

--- src/test/java/hu/masterfield/pages/LogOutPage.java
+++ src/test/java/hu/masterfield/pages/LogOutPage.java
@@ -2,11 +2,14 @@ package hu.masterfield.pages;
 
 import com.codeborne.selenide.SelenideElement;
 
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
 public class LogOutPage {
     public void logout() {
-        SelenideElement logoutBtn = $("#utility-header-logout-link");
+        SelenideElement logoutBtn = $("#utility-header-logout-link").shouldBe(visible, Duration.ofSeconds(10));
         logoutBtn.click();
     }
 }
